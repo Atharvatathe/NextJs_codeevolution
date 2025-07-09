838c991a953a783cf1840ccddbacad4ad477f120
Atharva Tathe <72079840+Atharvatathe@users.noreply.github.com>
2025-07-09 10:56:08 +0000

    default not found page for reviews and usePathname hook

--- routing-demo/src/app/products/[productId]/reviews/[reviewsId]/page.tsx
+++ routing-demo/src/app/products/[productId]/reviews/[reviewsId]/page.tsx
@@ -1,10 +1,14 @@
+import { notFound } from "next/navigation";
+
 export default async function ProductReview({
   params,
 }: {
   params: Promise<{ productId: string; reviewsId: string }>;
 }) {
   const { productId, reviewsId } = await params;
-  console.log(await params);
+  if (parseInt(reviewsId) > 1000) {
+    notFound();
+  }
   return (
     <h1>
       Review {reviewsId} for product {productId}
