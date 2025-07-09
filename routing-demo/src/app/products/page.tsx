19fd61d4283feac523cda50359e9f7087de4ac7f
Atharva Tathe <72079840+Atharvatathe@users.noreply.github.com>
2025-07-09 09:54:15 +0000

    Dynamic Routes

---
+++ routing-demo/src/app/products/page.tsx
@@ -0,0 +1,10 @@
+export default function ProductList () {
+    return(
+        <>
+        <h1>Product List</h1>
+        <h2>Produc 1</h2>
+        <h2>Produc 2</h2>
+        <h2>Produc 3</h2>
+        </>
+    )
+}
