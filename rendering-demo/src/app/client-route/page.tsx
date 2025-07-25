f4478d9adeb8c19662ec673058b1d4566bf7bc46
Atharva Tathe <72079840+Atharvatathe@users.noreply.github.com>
2025-07-25 07:18:13 +0000

    context provider

--- rendering-demo/src/app/client-route/page.tsx
+++ rendering-demo/src/app/client-route/page.tsx
@@ -3,18 +3,20 @@
 import Slider from "react-slick";
 import { serverSideFunction } from "../utils/server-utils";
 import React from "react";
+import { useTheme } from "@/components/theme-provider";
 // import Slider from "react-slick";
 // import "slick-carousel/slick/slick.css";
 // import "slick-carousel/slick/slick-theme.css";
 
 export default function ClientRoutePage() {
+   const theme = useTheme();
   // const result = serverSideFunction();
   //  const settings = {
   //   dots: true,
   // };
   return (
     <>
-    <div>
+    <div style={{ color: theme.colors.primary }}>
       <h1>Client route page</h1>
     </div>
       {/* <div className="image-slider-container">
